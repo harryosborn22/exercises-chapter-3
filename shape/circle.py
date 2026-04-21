0db73dbb4d2cb9241366b622ff93f37921b13900
harryosborn22 <harry.osborn22@imperial.ac.uk>
2026-04-21 15:49:51 +0100

    flake8 done

--- shape/circle.py
+++ shape/circle.py
@@ -1,9 +1,10 @@
 class Circle:
-    
+
     def __init__(self, centre, radius):
         self.centre = centre
         self.radius = radius
-    
+
     def __contains__(self, other):
-        diff = ((other[0] - self.centre[0])**2 + (other[1] - self.centre[1])**2)**0.5
-        return diff<self.radius
+        diff = ((other[0] - self.centre[0])**2 +
+                (other[1] - self.centre[1])**2)**0.5
+        return diff < self.radius
